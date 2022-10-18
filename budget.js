57b4cdc5f988f7e68fe600d72d2b02c5750e450d
Sumayea Rahman <shorbonam104@gmail.com>
2022-10-18 20:26:21 +0600

    more js

--- budget.js
+++ budget.js
@@ -5,6 +5,18 @@ document.getElementById('btn-calculate').addEventListener('click', function(){
     // console.log(inputAmountString);
     const newInputAmount = parseFloat(inputAmountString);
     console.log(newInputAmount);
+
+// clear value
+// inputField.value = '';
+
+    const inputFieldElement = document.getElementById("total-Added");
+    const previousInputTotalString = inputFieldElement.innerText;
+    const previousInputTotal = parseFloat(previousInputTotalString);
+    console.log(previousInputTotal)
+
+
+    
+
 })
 
 
