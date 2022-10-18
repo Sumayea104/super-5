240273c74a51531a859c9d3810885591cd190a5d
Sumayea Rahman <shorbonam104@gmail.com>
2022-10-18 18:48:08 +0600

    addition

---
+++ budget.js
@@ -0,0 +1,18 @@
+document.getElementById('btn-calculate').addEventListener('click', function(){
+    // console.log("can")
+    const inputField = document.getElementById('input-field');
+    const inputAmountString = inputField.value;
+    // console.log(inputAmountString);
+    const newInputAmount = parseFloat(inputAmountString);
+    console.log(newInputAmount);
+})
+
+
+
+
+
+
+
+document.getElementById('btn-calculateTotal').addEventListener('click', function(){
+    // console.log("canty")
+})
